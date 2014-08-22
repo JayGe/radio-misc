781768ab3b8c05a16322e0f87c86831c14a7fff9
Jay Ge <github@jaygee.plus.com>
2014-08-22 12:23:01 +0100

    stat output change

--- tobin1.py
+++ tobin1.py
@@ -9,7 +9,7 @@ parser=argparse.ArgumentParser(description="Takes a wav file and sets channel 1 
 parser.add_argument('-i', '--infile', help='WAV file to read from', required=True)
 parser.add_argument('-o', '--outfile', help='WAV file to write to', required=True)
 parser.add_argument('-s', '--stats', help='Displays some stats', action='store_true')
-parser.add_argument('-t', '--timespace', help='sample spacing', type=int, default=34)
+parser.add_argument('-t', '--timespace', help='sample spacing, default 34', type=int, default=34)
 
 args = vars(parser.parse_args())
 
@@ -27,7 +27,8 @@ snd = snd / (2.**15) # convert to -1 to 1
 
 state = 0
 statecount = 0
-
+binsection = 0
+binfull = ""
 for x in range(0, len(snd)):
 	statecount = statecount + 1
 	# working with one channel only as it lets us see the original plus altered at same time
@@ -41,11 +42,16 @@ for x in range(0, len(snd)):
 		cstate = 0
 	if cstate != state:
 		if args['stats']:
-			statediv = (statecount / timeSpace)
-			print "State switch at", x , state , "->" , cstate, "samples since last switch", statecount, statediv
-			print str(state) * statediv
+			statediv = (statecount / timeSpace) # work out the number of samples that fit in to the statecount
+			print "State switch at", x , state , "->" , cstate, "samples since last switch", statecount, "possible count", statediv
+			binsection = str(state) * statediv 
+#			print binsection		
+			binfull += binsection
 		state = cstate
 		statecount = 0
+
+if args['stats']:
+	print "The whole lot: " + binfull
 
 print 'Writing to: ' + outFile
 wavfile.write(outFile, sampFreq, snd)
